49a38b99644d50122de9e4c59d8191fb61b0f732
curtiFacundo <fcurti@frba.utn.edu.ar>
2024-04-04 00:00:19 +0000

    cambiar el utils.c

--- kernel/src/utils.c
+++ kernel/src/utils.c
@@ -2,11 +2,3 @@
 #include <stdio.h>
 #include <utils/hello.h>
 #include "main.h"
-
-int main(int argc, char* argv[]) {
-    t_log* logger;
-    logger = log_create("logsKernel.log","logsKernel",true,LOG_LEVEL_INFO);
-	log_info(logger,"Hola! Kernel");
-    log_destroy(logger);
-    return 0;
-}
