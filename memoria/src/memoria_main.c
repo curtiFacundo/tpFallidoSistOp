b79df4d6af7adb8bc0978683b6e986b8191cbc45
santi12386 <santiagoozan97@gmail.com>
2024-04-17 23:02:55 -0300

    fix memoria cliente

--- memoria/src/memoria_main.c
+++ memoria/src/memoria_main.c
@@ -17,7 +17,8 @@ int main(void) {
 	char* puertoCPU;
 	char* valor;
 	t_config* config;
-	t_paquete* save_handshake;
+	t_paquete* paquete_handshake_kernel;
+	t_paquete* paquete_handshake_cpu;
 	    
 	/*
 	TO DO list:
@@ -26,31 +27,32 @@ int main(void) {
 
 	logger = log_create("memoria.log", "memoria", 1, LOG_LEVEL_DEBUG);
 	config = config_create("../utils/config/memoria.config");
+	valor = config_get_string_value(config, "CLAVE");
 	//conexión
 
-	valor = config_get_string_value(config, "CLAVE");
-	save_handshake = crear_paquete(HANDSHAKE);
-	agregar_a_paquete (save_handshake, valor, strlen(valor)+1);
-
 	//-- COMIENZA CLIENTE PARA KERNEL
+	paquete_handshake_kernel = crear_paquete(HANDSHAKE);
+	agregar_a_paquete (paquete_handshake_kernel, valor, strlen(valor)+1);	
 	ipKernel = config_get_string_value(config, "IPKERNEL");
     puertoKernel = config_get_string_value(config, "PUERTOKERNEL");
 
 	conexionKernel = crear_conexion(ipKernel, puertoKernel, logger);
 
-	enviar_paquete(save_handshake, conexionKernel);
-	eliminar_paquete(save_handshake);
+	enviar_paquete(paquete_handshake_kernel, conexionKernel);
+	eliminar_paquete(paquete_handshake_kernel);
 	liberar_conexion(conexionKernel);
 	//-- FINALIZA CLIENTE PARA KERNEL
 
 	//-- COMIENZA CLIENTE PARA CPU
+	paquete_handshake_cpu = crear_paquete(HANDSHAKE);
+	agregar_a_paquete (paquete_handshake_cpu, valor, strlen(valor)+1);
 	ipCPU = config_get_string_value(config, "IPCPU");
     puertoCPU = config_get_string_value(config, "PUERTOCPU");
 
 	conexionCpu = crear_conexion(ipCPU, puertoCPU, logger);
 
-	enviar_paquete(save_handshake, conexionCpu);
-	eliminar_paquete(save_handshake);
+	enviar_paquete(paquete_handshake_kernel, conexionCpu);
+	eliminar_paquete(paquete_handshake_kernel);
 	//-- FINALIZA CLIENTE PARA CPU
 
 }
