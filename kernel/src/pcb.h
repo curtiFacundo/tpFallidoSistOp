eddc5b5c58b9e7bf66399bf71a6e0cbb0769c666
AlekTremoulet <atremoulet@frba.utn.edu.ar>
2024-05-19 20:28:59 +0000

    creación de PCB v0.1

--- kernel/src/pcb.h
+++ kernel/src/pcb.h
@@ -1,3 +1,4 @@
+#include  <stdint.h> // asegura que usamos tipos de enteros estándar 
 typedef enum  
 {
     NEW,
@@ -8,8 +9,23 @@ typedef enum
 }t_estado;
 
 typedef struct{
-    int id;
-    int* pc_id;
-    int quantum;
-    int estado;
+    uint32_t eax;
+    uint32_t ebx;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t ebp;
+    uint32_t esp;
+}RegistroCPU; // dice registros generales, supongo que se refiere a hacer esto :calavera:
+typedef struct{
+    int pid; // id del proceso
+    int* pc_id; // id program counter 
+    int quantum; 
+    int estado; // t_estado
+    t_list* instrucciones;
+    RegistroCPU registros;
 }pcb;
+
+
+pcb* crear_pcb(int pid, int pc_id, int quantum, RegistroCPU registros, t_list* instrucciones);
